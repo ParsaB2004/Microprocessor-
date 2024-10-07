2ad20c5bfe9ce7ae57cf0d4c686d523da5399ef9
ParsaB2004 <messiparsa83@gmail.com>
2024-10-07 10:32:51 -0700

    Add files via upload

---
+++ project1.ino
@@ -0,0 +1,16 @@
+//parsa behjati
+//session1
+
+int led = 9;
+
+void setup() {
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led , LOW);
+  delay(1000);
+}
